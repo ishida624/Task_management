bafc2200fb04ff1fd067e24e374f892b6d9fa40e
Gill <ishida624@gmail.com>
2020-09-21 17:47:50 +0800

    fix bug: GroupsController, only relation can use store() and delete()

--- app/Http/Controllers/api/GroupController.php
+++ app/Http/Controllers/api/GroupController.php
@@ -10,17 +10,26 @@ use App\Users;
 
 class GroupController extends Controller
 {
+    #顯示使用者所有的Groups
     public function index(Request $request)
     {
         $userData = $request->userData;
         $groups = $userData->ShowGroups;
         return $groups;
     }
+
+    #顯示card 所有的使用者
     public function card_users($cardId)
     {
-        $users = Card::find($cardId)->ShowUsers;
+        $card = Card::find($cardId);
+        if (!$card) {
+            return response()->json(['status' => false, 'error' => 'card search not found'], 400);
+        }
+        $users = $card->ShowUsers;
         return $users;
     }
+
+    #新增user 進來群組
     public function store(Request $request, $cardId)
     {
         $userData = $request->userData;
@@ -30,22 +39,33 @@ class GroupController extends Controller
         }
         // $cardOwner = $card->create_user;
         $userId = $request->user_id;
-        $group = Groups::create(['users_id' => $userId, 'card_id' => $cardId]);
-        return response()->json(['status' => true, 'group_data' => $group]);
+        $addUser = Users::find($userId);
+        if (!$addUser) {
+            return response()->json(['status' => false, 'error' => 'user search not found'], 400);
+        }
+        $group = $card->ShowGroups->where('users_id', $userId)->first();
+        if (isset($group)) {
+            return response()->json(['status' => false, 'error' => 'user is already in card'], 400);
+        }
+        $create = Groups::create(['users_id' => $userId, 'card_id' => $cardId]);
+        return response()->json(['status' => true, 'group_data' => $create]);
     }
 
+    #從卡片中刪除使用者
     public function delete_user(Request $request, $cardId)
     {
         $userData = $request->userData;
         $user = $userData->username;
         $deleteUserId = $request->user_id;
         // $card = Card::find($cardId)->ShowGroups;
-        $card = Groups::where('card_id', $cardId)->get();
-
+        $card = $userData->ShowCards->find($cardId);
+        // dd($card);
         if (!$card) {
             return response()->json(['status' => false, 'error' => 'card search not found'], 400);
         }
-        $cardOwner = $userData->ShowCards->find($cardId)->create_user;
+        $cardOwner = $card->create_user;
+        $cardGroup = $card->ShowGroups;
+        // dd($cardOwner);
         # 先判斷使用者是否為卡片創立者
         if ($user  == $cardOwner) {
             $Lv = 1;
@@ -54,13 +74,15 @@ class GroupController extends Controller
         }
 
         // dd($Lv);
-        $delete = $card->where('users_id', $deleteUserId)->first();
+        $delete = $cardGroup->where('users_id', $deleteUserId)->first();
+        // dd($delete);
+        if (!$delete) {
+            return response()->json(['status' => false, 'error' => 'user search not found'], 400);
+        }
         $deleteUser = Users::find($deleteUserId)->username;
+        // dd($deleteUser);
         if ($deleteUser == $cardOwner && $Lv == 2) {
             return response()->json(['status' => false, 'error' => 'you can not delete card owner'], 400);
-        }
-        if (!$deleteUser) {
-            return response()->json(['status' => false, 'error' => 'user search not found'], 400);
         }
         // dd($deleteUser);
         $delete->delete();
