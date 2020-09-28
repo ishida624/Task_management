e4da7c649ba36e95665bb793f4e9aacf6c590387
Gill <ishida624@gmail.com>
2020-09-28 11:30:17 +0800

    add middlewaer response log,kernel add in global

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\LogsInfo::class,
+        \App\Http\Middleware\ResponseLog::class,
     ];
 
     /**
