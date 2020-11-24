75364c8417a8ca4d62fdb12758176fec01e7586d
Gill <ishida624@gmail.com>
2020-11-24 16:46:11 +0800

    update model Users add GetToken function

--- app/Users.php
+++ app/Users.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Users extends Model
 {
@@ -11,9 +12,9 @@ class Users extends Model
     public $primaryKey = 'id';
     // public $timestamps = false;
     protected $fillable = ['username', 'password', 'email', 'image', 'remember_token', 'oauth'];
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token',];
+    protected $casts = ['oauth' => 'boolean',];
+
     public function ShowCards()
     {
         return $this->belongsToMany('App\Card', 'groups', 'users_id', 'card_id');
@@ -21,5 +22,18 @@ class Users extends Model
     public function ShowGroups()
     {
         return $this->hasMany('App\Groups');
+    }
+    public function GetToken()
+    {
+        do {
+            $token = Str::random(15);
+            $tokenCheck = Users::where('remember_token', $token)->first();
+            if (isset($tokenCheck)) {
+                $sameToken = true;
+            } else {
+                $sameToken = false;
+            }
+        } while ($sameToken);
+        return $token;
     }
 }
